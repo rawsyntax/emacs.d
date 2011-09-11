2952f85d492d3c75fd15bd1053c070daaa0c6a1c
Eric Himmelreich <eric@rawsyntax.com>
2011-09-10 22:59:48 -0400

    ui functions and bindings

--- eric/bindings.el
+++ eric/bindings.el
@@ -1,1 +1,10 @@
 (global-set-key "\C-cm" 'magit-status)
+(global-set-key (kbd "\C-cu") 'reset-ui)
+
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+(global-set-key (kbd "C-c M-x") 'smex-update-and-run)
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+
+(global-set-key (kbd "C-w") 'backward-kill-word)
+(global-set-key (kbd "C-x C-k") 'kill-region)
--- eric/ui.el
+++ eric/ui.el
@@ -6,3 +6,67 @@
                                                          exit-minibuffer
                                                          keyboard-quit))
                              (ding))))
+
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(global-hl-line-mode 1)
+(blink-cursor-mode 1)
+
+(setq-default show-trailing-whitespace t)
+
+;;default display position/size based on display resolution
+;;warning: assumption that the 24/22 " displays are oriented
+;;         above the laptop's display
+;;         Haven't found a way to check multiple monitor
+;;         relative orientation via emacs yet...
+;;         NOTE: try integrating with applescript for this?
+(defun reset-ui ()
+  (interactive)
+  (smex-initialize)
+  (require 'color-theme)
+  (require 'color-theme-solarized)
+  (delete-other-windows)
+  (color-theme-solarized-dark)
+  (set-cursor-color "deeppink")
+  (set-face-background 'modeline-inactive "gray10")
+  ;;(set-face-background 'hl-line "gray20")
+  (cond
+   ((= 1050 (display-pixel-height)); 22" display
+    (set-frame-size (selected-frame) 165 71)
+    (set-frame-position (selected-frame) 0 -1050))
+   ((= 1200 (display-pixel-height)); 24" display
+    (set-frame-size (selected-frame) 165 76)
+    (set-frame-position (selected-frame) 0 -1200))
+   (t ; laptop runs 1440x900
+    (set-frame-size (selected-frame) 165 53)
+    (set-frame-position (selected-frame) 150 0)))
+  (split-window-horizontally))
+
+
+(defun emacs-client-setup ()
+  "Set the look and feel, different for terminal vs GUI emacsclient"
+  (if (window-system)
+      (spartan-gui)
+    (spartan-cli)))
+
+(if (daemonp)
+    (progn
+      (defun spartan-gui ()
+        "Restore the look and feel that running in daemon mode cannot account
+for when attached via a GUI client."
+        (setq ns-command-modifier 'meta)
+        ;; restore look and feel
+        (reset-ui))
+      (defun spartan-cli ()
+        "Restore the look and feel that running in daemon mode cannot account
+for when attached via a commandline client."
+        (set-cursor-color "deeppink")
+        )
+      ))
+
+;; fires when an emacs frame is created (emacsclient)
+(add-hook 'server-visit-hook 'emacs-client-setup)
+
+;; hook for setting up UI when not running in daemon mode
+(add-hook 'emacs-startup-hook 'reset-ui)
