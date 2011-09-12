2c5641c0c3812c62e8909994339c59c8f8cc97f6
Eric Himmelreich <eric@rawsyntax.com>
2011-09-12 11:06:47 -0400

    some left over defuns from my old emacs config

---
+++ eric/defuns.el
@@ -0,0 +1,42 @@
+(defun delete-file-and-buffer ()
+  "Deletes the current file and buffer, assumes file exists"
+  (interactive)
+  (delete-file buffer-file-name)
+  (kill-buffer (buffer-name)))
+
+(defun refresh-buffer ()
+  "Refresh the current buffer from disk"
+  (interactive)
+  (revert-buffer t t))
+
+(defun rename-file-and-buffer (new-name)
+  "Renames both current buffer and file it's visiting to NEW-NAME."
+  (interactive "sNew name: ")
+  (let ((name (buffer-name))
+        (filename (buffer-file-name)))
+    (if (not filename)
+        (message "Buffer '%s' is not visiting a file!" name)
+      (if (get-buffer new-name)
+          (message "A buffer named '%s' already exists!" new-name)
+        (progn
+          (rename-file name new-name 1)
+          (rename-buffer new-name)
+          (set-visited-file-name new-name)
+          (set-buffer-modified-p nil))))))
+
+(defun swap-windows ()
+ "If you have 2 windows, it swaps them."
+ (interactive)
+ (cond ((not (= (count-windows) 2))
+        (message "You need exactly 2 windows to do this."))
+       (t
+        (let* ((w1 (first (window-list)))
+               (w2 (second (window-list)))
+               (b1 (window-buffer w1))
+               (b2 (window-buffer w2))
+               (s1 (window-start w1))
+               (s2 (window-start w2)))
+          (set-window-buffer w1 b2)
+          (set-window-buffer w2 b1)
+          (set-window-start w1 s2)
+          (set-window-start w2 s1)))))
