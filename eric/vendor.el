32b522cd861e5cf66f5f6186784edc906be67909
Eric Himmelreich <eric@rawsyntax.com>
2011-11-08 13:24:23 -0500

    more generically load external dependencies

--- eric/vendor.el
+++ eric/vendor.el
@@ -1,7 +1,14 @@
 ;; this is where I require non-elpa external dependencies
-(add-to-list 'load-path (concat dotfiles-dir "vendor/thorstadt-haml-mode"))
-(require 'haml-mode)
+(setq eawh-elisp-external-dir
+      (expand-file-name "vendor" dotfiles-dir))
 
+
+;; add external projects to load path
+(dolist (project (directory-files eawh-elisp-external-dir t "\\w+"))
+  (when (file-directory-p project)
+    (add-to-list 'load-path project)))
+
+;; marmalade's haml-mode does not work with emacs 24
+(require 'haml-mode)
 ;; marmalade's inf-ruby is 2.2.1 and does not work
-(add-to-list 'load-path (concat dotfiles-dir "vendor/inf-ruby"))
 (require 'inf-ruby)
