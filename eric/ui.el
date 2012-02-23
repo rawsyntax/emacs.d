696733aaa46691c4f0f74aae7e6a8426fcac3a5e
Eric Himmelreich <eric@rawsyntax.com>
2012-02-23 16:54:10 -0500

    ffap stop pinging my ruby method calls (mistaking for country tlds)

--- eric/ui.el
+++ eric/ui.el
@@ -8,6 +8,7 @@
                                            keyboard-quit))
                              (ding))))
 
+(setq ffap-machine-p-known 'reject)
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 (menu-bar-mode -1)
