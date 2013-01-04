e6ca5bb9cd130fdacdcaecd33d8f11209bb1a41e
Eric Himmelreich <eric@rawsyntax.com>
2013-01-04 17:53:41 -0500

    use osx's spotlight instead of locate

--- Blondie.local.el
+++ Blondie.local.el
@@ -1,6 +1,9 @@
 ;; mac modifications
 (setq mac-command-modifier 'meta)
 
+;; use OS X's Spotlight for M-x locate
+(setq locate-make-command-line (lambda (s) `("mdfind" "-name" ,s)))
+
 ;; magit can't find git when Emacs.app is launched by launchd
 (setenv "PATH" (concat "/usr/local/bin:" (getenv "PATH")))
 (push "/usr/local/bin" exec-path)
