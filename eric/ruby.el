c8e2fd8eb25a85baa2ecb622c7866f8e6381eb95
Eric Himmelreich <eric@rawsyntax.com>
2013-03-27 13:35:30 -0400

    other Filefiles are also ruby-mode

--- eric/ruby.el
+++ eric/ruby.el
@@ -1,2 +1,4 @@
 (remove-hook 'ruby-mode-hook 'esk-run-coding-hook)
 (add-to-list 'auto-mode-alist '("Guardfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Berksfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Cheffile" . ruby-mode))
