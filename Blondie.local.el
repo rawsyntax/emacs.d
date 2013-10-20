196c06aabc7189a00e8a944c4418a0f067703777
Eric Himmelreich <eric@rawsyntax.com>
2013-10-20 13:17:42 -0400

    Fix pathing issues due to how emacs gets started

--- Blondie.local.el
+++ Blondie.local.el
@@ -7,5 +7,8 @@
 ;; magit can't find git when Emacs.app is launched by launchd
 (setenv "PATH" (concat "/usr/local/bin:" (getenv "PATH")))
 (push "/usr/local/bin" exec-path)
+;; boxen stores emacs binaries elsewhere
+(push "/opt/boxen/homebrew/Cellar/emacs/24.3/bin/" exec-path)
+(exec-path-from-shell-initialize)
 
 (add-to-list 'ido-ignore-files "\\`\\.DS_Store")
