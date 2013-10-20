e4c6f1014bc9b45ef34582fb6fd8f4dbb2e6065d
Eric Himmelreich <eric@rawsyntax.com>
2013-10-20 13:26:31 -0400

    Disable autopair when in multi-term

--- eric/multi-term.el
+++ eric/multi-term.el
@@ -1,5 +1,7 @@
 (setq multi-term-program "/bin/zsh")
 
-(add-hook 'term-mode-hook (lambda () (setq show-trailing-whitespace nil)))
+(add-hook 'term-mode-hook (lambda ()
+                            (setq show-trailing-whitespace nil)
+                            (autopair-mode -1)))
 (autoload 'multi-term "multi-term" nil t)
 (autoload 'multi-term-next "multi-term" nil t)
