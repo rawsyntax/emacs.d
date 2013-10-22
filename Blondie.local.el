2e5fa33125173cd67d5c0542d93e239ac790bfa0
Eric Himmelreich <eric@rawsyntax.com>
2013-10-22 12:19:37 -0400

    exec-path-from-shell-initialize handles pathing issues

--- Blondie.local.el
+++ Blondie.local.el
@@ -4,11 +4,6 @@
 ;; use OS X's Spotlight for M-x locate
 (setq locate-make-command-line (lambda (s) `("mdfind" "-name" ,s)))
 
-;; magit can't find git when Emacs.app is launched by launchd
-(setenv "PATH" (concat "/usr/local/bin:" (getenv "PATH")))
-(push "/usr/local/bin" exec-path)
-;; boxen stores emacs binaries elsewhere
-(push "/opt/boxen/homebrew/Cellar/emacs/24.3/bin/" exec-path)
 (exec-path-from-shell-initialize)
 
 (add-to-list 'ido-ignore-files "\\`\\.DS_Store")
