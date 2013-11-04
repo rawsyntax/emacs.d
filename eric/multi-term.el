41b723ec14c4859e934e8694da0a94bc3d0a121f
Eric Himmelreich <eric@rawsyntax.com>
2013-11-04 01:22:16 -0500

    cleanup multi-term setup

--- eric/multi-term.el
+++ eric/multi-term.el
@@ -1,11 +1,10 @@
 (setq multi-term-program "/bin/zsh")
 
-(add-hook 'term-mode-hook (lambda ()
-                            (setq show-trailing-whitespace nil)
-                            (autopair-mode -1)))
-
 (add-hook 'term-mode-hook
-  (lambda ()
-    (add-to-list 'term-bind-key-alist '("M-[" . multi-term-next))
-    (add-to-list 'term-bind-key-alist '("M-]" . multi-term-prev))
-    (add-to-list 'term-bind-key-alist '("C-c C-M-t" .  multi-term))))
+          (lambda ()
+            (setq show-trailing-whitespace nil)
+            (autopair-mode -1)
+            (add-to-list 'term-bind-key-alist '("M-[" . multi-term-prev))
+            (add-to-list 'term-bind-key-alist '("M-]" . multi-term-next))
+            (setq term-buffer-maximum-size 10000)
+            (define-key term-raw-map (kbd "C-y") 'term-paste)))
