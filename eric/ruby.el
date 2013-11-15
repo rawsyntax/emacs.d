b47c16d7863e57a1d200276874114c9c80365bef
Eric Himmelreich <eric@rawsyntax.com>
2013-11-15 15:10:59 -0500

    update inf-ruby

--- eric/ruby.el
+++ eric/ruby.el
@@ -1,6 +1,4 @@
 (remove-hook 'ruby-mode-hook 'esk-run-coding-hook)
-;; https://github.com/technomancy/emacs-starter-kit/pull/145
-(defalias 'inf-ruby-keys 'inf-ruby-setup-keybindings)
 (add-to-list 'auto-mode-alist '("Guardfile" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Berksfile" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Cheffile" . ruby-mode))
