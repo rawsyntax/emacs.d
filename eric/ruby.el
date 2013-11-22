5770b5a5bf60d345e05b180c287f8a69d0570cae
Eric Himmelreich <eric@rawsyntax.com>
2013-11-22 10:51:57 -0500

    quick fix for inf-ruby-keys not existing

--- eric/ruby.el
+++ eric/ruby.el
@@ -1,4 +1,9 @@
 (remove-hook 'ruby-mode-hook 'esk-run-coding-hook)
+
+;; fix for https://github.com/nonsequitur/inf-ruby/commit/86db14c
+(remove-hook 'ruby-mode-hook 'inf-ruby-keys)
+(add-hook 'ruby-mode-hook 'inf-ruby-setup-keybindings)
+
 (add-to-list 'auto-mode-alist '("Guardfile" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Berksfile" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Cheffile" . ruby-mode))
