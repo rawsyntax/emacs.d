f8797ed9e0b4d42932da350abf6726c7fc04fbaa
Eric Himmelreich <eric@rawsyntax.com>
2014-02-12 23:02:57 -0500

    Fix git commit wrapping, live with it in text mode

--- eric/text-mode.el
+++ eric/text-mode.el
@@ -1,6 +1,4 @@
 ;; soft wrap lines based on window width, like a normal prose writing editor
-(remove-hook 'text-mode-hook 'turn-on-auto-fill)
-(add-hook 'git-commit-mode 'turn-on-auto-fill)
 (add-hook 'text-mode-hook 'turn-on-visual-line-mode)
 
 ;; indicate soft wrapped lines vs actually hard line breaks
