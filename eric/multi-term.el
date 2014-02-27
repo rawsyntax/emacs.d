8e0d3c2bc9f93f04db0771422d59e738c75c4728
Eric Himmelreich <eric@rawsyntax.com>
2014-02-27 00:07:18 -0500

    Turn off some modes in term, increase buffer

--- eric/multi-term.el
+++ eric/multi-term.el
@@ -3,10 +3,12 @@
 (add-hook 'term-mode-hook
           (lambda ()
             (compilation-shell-minor-mode 1)
+            (show-paren-mode -1)
+            (tooltip-mode -1)
             (setq yas-dont-activate t)
             (setq show-trailing-whitespace nil)
             (autopair-mode -1)
             (add-to-list 'term-bind-key-alist '("M-[" . multi-term-prev))
             (add-to-list 'term-bind-key-alist '("M-]" . multi-term-next))
-            (setq term-buffer-maximum-size 6000)
+            (setq term-buffer-maximum-size 50000)
             (define-key term-raw-map (kbd "C-y") 'term-paste)))
