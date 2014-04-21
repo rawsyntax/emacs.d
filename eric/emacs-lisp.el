c762892bc7d6128762dec870e08f036ef29e3fe3
Eric Himmelreich <eric@rawsyntax.com>
2014-04-20 23:14:46 -0400

    Remove stale *.elc files

---
+++ eric/emacs-lisp.el
@@ -0,0 +1,11 @@
+;; thanks http://emacsredux.com/blog/2013/06/25/boost-performance-by-leveraging-byte-compilation/
+(defun remove-elc-on-save ()
+  "If you're saving an elisp file, likely the .elc is no longer valid."
+  (add-hook 'after-save-hook
+            (lambda ()
+              (if (file-exists-p (concat buffer-file-name "c"))
+                  (delete-file (concat buffer-file-name "c"))))
+            nil
+            t))
+
+(add-hook 'emacs-lisp-mode-hook 'remove-elc-on-save)
