7d2f1025c0e18ca3c44c5fb5f42c89b5d2ecc589
Eric Himmelreich <eric@rawsyntax.com>
2014-04-22 11:35:11 -0400

    Move rhtml-mode customizations to proper file

--- eric/vendor.el
+++ eric/vendor.el
@@ -3,13 +3,10 @@
       ;; TODO: don't hardcode this
       "/Users/eric/.emacs.d/vendor")
 
-
 ;; add external projects to load path
 (dolist (project (directory-files eawh-elisp-external-dir t "\\w+"))
   (when (file-directory-p project)
     (add-to-list 'load-path project)))
 
-;; marmalade's version is broken for emacs 24
 (require 'haml-mode)
-(add-to-list 'auto-mode-alist '("\\.erb$" . rhtml-mode))
 (require 'ag-and-a-half)
