e4994fdbba239f164ff4c20722de153a12fbc569
Eric Himmelreich <eric@rawsyntax.com>
2014-04-22 12:32:23 -0400

    Enhance delete / rename file and buffer

--- eric/defuns.el
+++ eric/defuns.el
@@ -1,30 +1,35 @@
-(defun delete-file-and-buffer ()
-  "Deletes the current file and buffer, assumes file exists"
-  (interactive)
-  (delete-file buffer-file-name)
-  (kill-buffer (buffer-name)))
-
 (defun refresh-buffer ()
   "Refresh the current buffer from disk"
   (interactive)
   (revert-buffer t t))
 
-;; TODO: make rename uniquify buffer if a buffer with same name
-;;       already exists
-(defun rename-file-and-buffer (new-name)
-  "Renames both current buffer and file it's visiting to NEW-NAME."
-  (interactive "sNew name: ")
-  (let ((name (buffer-name))
-        (filename (buffer-file-name)))
-    (if (not filename)
-        (message "Buffer '%s' is not visiting a file!" name)
-      (if (get-buffer new-name)
-          (message "A buffer named '%s' already exists!" new-name)
-        (progn
-          (rename-file name new-name 1)
-          (rename-buffer new-name)
-          (set-visited-file-name new-name)
-          (set-buffer-modified-p nil))))))
+;; thanks https://github.com/bbatsov/prelude for vc integration
+(defun rename-file-and-buffer ()
+  "Renames current buffer and file it is visiting."
+  (interactive)
+  (let ((filename (buffer-file-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (message "Buffer is not visiting a file!")
+      (let ((new-name (read-file-name "New name: " filename)))
+        (cond
+         ((vc-backend filename) (vc-rename-file filename new-name))
+         (t
+          (rename-file filename new-name t)
+          (set-visited-file-name new-name t t)))))))
+
+;; thanks https://github.com/bbatsov/prelude for vc integration
+(defun delete-file-and-buffer ()
+  "Kill the current buffer and deletes the file it is visiting."
+  (interactive)
+  (let ((filename (buffer-file-name)))
+    (when filename
+      (if (vc-backend filename)
+          (vc-delete-file filename)
+        (when (y-or-n-p (format "Are you sure you want to delete %s? " filename))
+          (delete-file filename)
+          (message "Deleted file %s" filename)
+          (kill-buffer))))))
+
 
 (defun swap-windows ()
  "If you have 2 windows, it swaps them."
