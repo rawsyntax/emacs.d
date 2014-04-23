14354959b1f6e4774aafc68a624a65f2ca1087a9
Eric Himmelreich <eric@rawsyntax.com>
2014-04-23 11:33:44 -0400

    Move find-file-at-point to its own file

---
+++ eric/ffap.el
@@ -0,0 +1,18 @@
+(require 'ffap)
+(defvar ffap-c-commment-regexp "^/\\*+"
+  "Matches an opening C-style comment, like \"/***\".")
+
+(defadvice ffap-file-at-point (after avoid-c-comments activate)
+  "Don't return paths like \"/******\" unless they actually exist.
+
+This fixes the bug where ido would try to suggest a C-style
+comment as a filename."
+  (ignore-errors
+    (when (and ad-return-value
+               (string-match-p ffap-c-commment-regexp
+                               ad-return-value)
+               (not (ffap-file-exists-string ad-return-value)))
+      (setq ad-return-value nil))))
+;; prevent ffap from trying to interpret code as domain names (and
+;; pinging sites that don't exist, which locks the UI until timeout)
+(setq ffap-machine-p-known 'reject)
--- eric/ui.el
+++ eric/ui.el
@@ -34,26 +34,6 @@
 ;; Highlight matching parentheses when the point is on them.
 (show-paren-mode 1)
 
-(require 'ffap)
-(defvar ffap-c-commment-regexp "^/\\*+"
-  "Matches an opening C-style comment, like \"/***\".")
-
-(defadvice ffap-file-at-point (after avoid-c-comments activate)
-  "Don't return paths like \"/******\" unless they actually exist.
-
-This fixes the bug where ido would try to suggest a C-style
-comment as a filename."
-  (ignore-errors
-    (when (and ad-return-value
-               (string-match-p ffap-c-commment-regexp
-                               ad-return-value)
-               (not (ffap-file-exists-string ad-return-value)))
-      (setq ad-return-value nil))))
-;; prevent ffap from trying to interpret code as domain names (and
-;; pinging sites that don't exist, which locks the UI until timeout)
-(setq ffap-machine-p-known 'reject)
-
-
 (set-default 'indent-tabs-mode nil)
 (set-default 'indicate-empty-lines t)
 (set-default 'imenu-auto-rescan t)
