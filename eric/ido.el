e53ea96727fc407692a331f4923e8c8dbddd607d
Eric Himmelreich <eric@rawsyntax.com>
2014-04-23 11:32:17 -0400

    Move ido setup to its own file

--- eric/ido.el
+++ eric/ido.el
@@ -1,1 +1,13 @@
-(setq ido-case-fold t)
+(ido-mode t)
+(ido-ubiquitous-mode)
+(setq ido-enable-prefix nil
+      ido-enable-flex-matching t
+      ido-auto-merge-work-directories-length nil
+      ido-create-new-buffer 'always
+      ido-use-filename-at-point 'guess
+      ido-use-virtual-buffers t
+      ido-handle-duplicate-virtual-buffers 2
+      ido-max-prospects 10
+      ido-case-fold t)
+
+(add-to-list 'ido-ignore-files "\\`\\.DS_Store")
--- eric/ui.el
+++ eric/ui.el
@@ -34,20 +34,6 @@
 ;; Highlight matching parentheses when the point is on them.
 (show-paren-mode 1)
 
-(ido-mode t)
-(ido-ubiquitous-mode)
-(setq ido-enable-prefix nil
-      ido-enable-flex-matching t
-      ido-auto-merge-work-directories-length nil
-      ido-create-new-buffer 'always
-      ido-use-filename-at-point 'guess
-      ido-use-virtual-buffers t
-      ido-handle-duplicate-virtual-buffers 2
-      ido-max-prospects 10)
-
-(add-to-list 'ido-ignore-files "\\`\\.DS_Store")
-
-
 (require 'ffap)
 (defvar ffap-c-commment-regexp "^/\\*+"
   "Matches an opening C-style comment, like \"/***\".")
