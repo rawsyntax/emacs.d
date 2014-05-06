a1dbf68af055281b8dcbd4864a7e1eee812ef851
Eric Himmelreich <eric@rawsyntax.com>
2014-05-06 12:32:21 -0400

    Fix term input mangled when altering command from history
    I can now use C-a to move to the beginning of line and add environment
    variables to a rake task.  For example:
    
    rake test
    (then later, via history)
    RACK_ENV=custom rake test
                   ^ cursor here when hitting RET
    
    Will not mangle the input on that line anymore

--- eric/multi-term.el
+++ eric/multi-term.el
@@ -19,14 +19,22 @@
 
 (ad-activate 'term-send-input)
 
+(defun term-eol-then-ret ()
+  (interactive)
+  (end-of-line)
+  (term-send-input))
+
 (add-hook 'term-mode-hook
           (lambda ()
             (compilation-shell-minor-mode 1)
             (setq yas-dont-activate t)
             (setq show-trailing-whitespace nil)
+            (setq term-eol-on-send nil)
             (autopair-mode -1)
             (add-to-list 'term-bind-key-alist '("M-[" . multi-term-prev))
             (add-to-list 'term-bind-key-alist '("M-]" . multi-term-next))
             (add-to-list 'term-bind-key-alist '("C-z" . term-send-raw))
+            (add-to-list 'term-unbind-key-list "RET")
+            (add-to-list 'term-bind-key-alist '("RET" . term-eol-then-ret))
             (setq term-buffer-maximum-size 50000)
             (define-key term-raw-map (kbd "C-y") 'term-paste)))
