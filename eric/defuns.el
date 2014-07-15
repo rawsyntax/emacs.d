2ae4cdd890018654c196d72213bf04694ce1d8a5
Eric Himmelreich <eric@rawsyntax.com>
2014-07-15 12:34:52 -0400

    Move forgotten starter kit function

--- eric/defuns.el
+++ eric/defuns.el
@@ -165,6 +165,16 @@
   (interactive)
   (insert (format-time-string "%c" (current-time))))
 
+(defun esk-eval-and-replace ()
+  "Replace the preceding sexp with its value."
+  (interactive)
+  (backward-kill-sexp)
+  (condition-case nil
+      (prin1 (eval (read (current-kill 0)))
+             (current-buffer))
+    (error (message "Invalid expression")
+           (insert (current-kill 0)))))
+
 (defun toggle-shell-visor ()
   "Brings up a visor like eshell buffer, filling the entire emacs frame"
   (interactive)
