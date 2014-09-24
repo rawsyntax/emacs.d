7c24df58016710f4512f6baf7be2d15f2016d424
Eric Himmelreich <eric@rawsyntax.com>
2014-09-24 17:42:14 -0400

    Disable wazzup mode in magit
    It freezes the UI when I hit it by accident

--- eric/magit.el
+++ eric/magit.el
@@ -1,2 +1,7 @@
 (setq magit-set-upstream-on-push t)
 (setq magit-default-tracking-name-function 'magit-default-tracking-name-branch-only)
+
+;; disable wazzup
+(add-hook 'magit-mode-hook
+          (lambda()
+            (local-unset-key (kbd "w"))))
