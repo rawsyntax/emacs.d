82563ceca19448ef7a712d6d29b2bd5af058f7b3
Eric Himmelreich <eric@rawsyntax.com>
2015-05-06 00:06:37 -0400

    Set default browser

--- Blondie.local.el
+++ Blondie.local.el
@@ -2,6 +2,7 @@
 (setq mac-command-modifier 'meta
       browse-url-browser-function 'browse-default-macosx-browser
       locate-make-command-line (lambda (s) `("mdfind" "-name" ,s))
-      projectile-indexing-method 'alien)
+      projectile-indexing-method 'alien
+      browse-url-browser-function 'browse-url-default-macosx-browser)
 
 (exec-path-from-shell-initialize)
