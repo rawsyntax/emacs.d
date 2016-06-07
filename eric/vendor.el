9188341b50584b6586a29dc86119b9d018cd458d
Eric Himmelreich <eric@rawsyntax.com>
2016-06-07 13:30:53 -0400

    Don't hardcode elisp vendor directory

--- eric/vendor.el
+++ eric/vendor.el
@@ -1,7 +1,6 @@
 ;; this is where I require non-elpa external dependencies
 (setq eawh-elisp-external-dir
-      ;; TODO: don't hardcode this
-      "/Users/eric/.emacs.d/vendor")
+      (expand-file-name (concat user-emacs-directory "vendor")))
 
 ;; add external projects to load path
 (dolist (project (directory-files eawh-elisp-external-dir t "\\w+"))
