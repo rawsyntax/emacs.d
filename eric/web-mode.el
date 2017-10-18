32d166afb3c38bfabbf560b784cca3765bdb9255
Eric Himmelreich <eric@rawsyntax.com>
2017-10-18 09:24:05 -0500

    Fix JSX indentation

--- eric/web-mode.el
+++ eric/web-mode.el
@@ -1,6 +1,16 @@
 (setq web-mode-code-indent-offset 2
-      web-mode-markup-indent-offset 2)
+      web-mode-css-indent-offset 2
+      web-mode-markup-indent-offset 2
+      web-mode-attr-indent-offset 2
+      web-mode-content-types-alist '(("jsx" . "\\.js[x]?\\'")))
+
+(add-hook 'web-mode-hook
+      (lambda ()
+        ;; short circuit js mode and just do everything in jsx-mode
+        (if (equal web-mode-content-type "javascript")
+            (web-mode-set-content-type "jsx")
+          (message "now set to: %s" web-mode-content-type))))
 
 (add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.js$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.erb$" . web-mode))
-
