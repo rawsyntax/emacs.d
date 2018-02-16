cc95cffc8957781c1f5e4c3b3563a721858ed7d3
Eric Himmelreich <eric@rawsyntax.com>
2018-02-16 11:01:17 -0600

    Add more extensions for web-mode

--- eric/web-mode.el
+++ eric/web-mode.el
@@ -13,4 +13,7 @@
 
 (add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.js$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.json$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.erb$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.scala\\.html$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.scss$" . web-mode))
