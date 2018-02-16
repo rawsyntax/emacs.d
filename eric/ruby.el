ca1969ca3bed340a58768b61e9a08005c0c1d838
Eric Himmelreich <eric@rawsyntax.com>
2018-02-16 11:01:32 -0600

    Add jb template as ruby mode

--- eric/ruby.el
+++ eric/ruby.el
@@ -5,9 +5,11 @@
 (remove-hook 'ruby-mode-hook 'inf-ruby-keys)
 
 (add-to-list 'auto-mode-alist '("\\.gemspec$" . enh-ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.rake$" . enh-ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.ru$" . enh-ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.thor$" . enh-ruby-mode))
+
+;; https://github.com/amatsuda/jb template
+(add-to-list 'auto-mode-alist '("\\.jb$" . enh-ruby-mode))
 
 (add-to-list 'auto-mode-alist '("/Berksfile$" . enh-ruby-mode))
 (add-to-list 'auto-mode-alist '("/Capfile$" . enh-ruby-mode))
