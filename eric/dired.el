7e28c26d6964e88f55ab5c64be9230cb0220c278
Eric Himmelreich <eric@rawsyntax.com>
2018-03-15 13:58:47 -0500

    Add easy sorting to dired

--- eric/dired.el
+++ eric/dired.el
@@ -1,2 +1,22 @@
 ;; requires `brew install coreutils` on os x
 (setq insert-directory-program (executable-find "gls"))
+
+;; thanks http://ergoemacs.org/emacs/dired_sort.html
+(defun xah-dired-sort ()
+  "Sort dired dir listing in different ways.
+Prompt for a choice.
+URL `http://ergoemacs.org/emacs/dired_sort.html'
+Version 2015-07-30"
+  (interactive)
+  (let ($sort-by $arg)
+    (setq $sort-by (ido-completing-read "Sort by:" '( "date" "size" "name" "dir")))
+    (cond
+     ((equal $sort-by "name") (setq $arg "-Al --si --time-style long-iso "))
+     ((equal $sort-by "date") (setq $arg "-Al --si --time-style long-iso -t"))
+     ((equal $sort-by "size") (setq $arg "-Al --si --time-style long-iso -S"))
+     ((equal $sort-by "dir") (setq $arg "-Al --si --time-style long-iso --group-directories-first"))
+     (t (error "logic error 09535" )))
+    (dired-sort-other $arg )))
+
+(define-key dired-mode-map (kbd "s") 'xah-dired-sort)
+
