1643f90326de35926b1d3289952913c23d459c2f
Eric Himmelreich <eric@rawsyntax.com>
2019-05-15 16:08:05 -0500

    Switch package repo to just melpa

--- init.el
+++ init.el
@@ -3,7 +3,7 @@
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (add-to-list 'package-archives
-             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+             '("melpa" . "https://melpa.org/packages/") t)
 
 (package-initialize)
 
